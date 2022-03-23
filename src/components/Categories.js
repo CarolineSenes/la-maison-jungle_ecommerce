dc0921b682e5a853e311c9760eb9fbc3afabe893
SENES <caro67200@gmail.com>
2022-03-23 22:21:10 +0100

    upDate Categorie : change default category name

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/Categories.css";
+import "../App.css"
 
 const Categories = ({ categories, activeCategory, setActiveCategory }) => {
   return (
     <div className="lmj-categories">
-      <select value={activeCategory} onChange={(e)=>setActiveCategory(e.target.value)} className="lmj-categories-select">
-        <option value=''>---</option>
+      <select value={activeCategory} onChange={(e)=>setActiveCategory(e.target.value)} className="lmj-select">
+        <option value=''>Toutes catégories</option>
         {categories.map((cat) => (
           <option value={cat} key={cat}>
             {cat}
