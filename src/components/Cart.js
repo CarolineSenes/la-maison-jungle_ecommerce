c8d53a50c93d586f3ec2fe797c886321fa1e9dfd
SENES <caro67200@gmail.com>
2022-03-23 22:16:34 +0100

    add feature Cart : increase/decrease plant quantity & remove plant

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,13 +3,11 @@ import "../styles/Cart.css";
 
 const Cart = ({ cart, updateCart }) => {
   const [isOpen, setIsOpen] = useState(true);
-  console.log(cart);
 
   const total = cart.reduce(
-		(acc, plantType) => acc + plantType.amount * plantType.price,
-		0
-	)
-
+    (acc, plantType) => acc + plantType.amount * plantType.price,
+    0
+  );
 
   function removePlant(name) {
     const currentPlantSaved = cart.find((plant) => plant.name === name);
@@ -19,6 +17,39 @@ const Cart = ({ cart, updateCart }) => {
         (plant) => plant.name !== name
       );
       updateCart([...cartFilteredCurrentPlant]);
+    }
+  }
+
+  function addToCart(name, price) {
+    //vérifie si plate est déjà présente dans le state "cart" (donc dans la liste du panier)
+    const currentPlantSaved = cart.find((plant) => plant.name === name);
+    //si la plante existe déjà, on créé un nouveau tableau sans elle
+    if (currentPlantSaved) {
+      const cartFilteredCurrentPlant = cart.filter(
+        (plant) => plant.name !== name
+      );
+      //met à jour le state "cart" avec les données du nouveau tableau "cartFilteredCurrentPlant" et on la rajoute (sous forme d'objet) en ajoutant 1 à sa quantité
+      updateCart([
+        ...cartFilteredCurrentPlant,
+        { name, price, amount: currentPlantSaved.amount + 1 },
+      ]);
+    } else {
+      updateCart([...cart, { name, price, amount: 1 }]);
+    }
+  }
+
+  function removeFromCart(name, price) {
+    const currentPlantSaved = cart.find((plant) => plant.name === name);
+    if (currentPlantSaved.amount !== 1) {
+      const cartFilteredCurrentPlant = cart.filter(
+        (plant) => plant.name !== name
+      );
+      updateCart([
+        ...cartFilteredCurrentPlant,
+        { name, price, amount: currentPlantSaved.amount - 1 },
+      ]);
+    } else {
+      removePlant(name)
     }
   }
 
@@ -34,11 +65,23 @@ const Cart = ({ cart, updateCart }) => {
       {cart.length > 0 ? (
         <div>
           <h2>Panier</h2>
-          <ul>
+          <ul className="lmj-cart-list">
             {cart.map(({ name, price, amount }, index) => (
-              <div key={`${name}-${index}`}>
-                {name} {price}€ x {amount}
-                <button onClick={() => removePlant(name)}>Supprimer</button>
+              <div className="lmj-cart-list-item" key={`${name}-${index}`}>
+                {name} {price}€{" "}
+                <ion-icon
+                  name="add"
+                  onClick={() => addToCart(name, price)}
+                ></ion-icon>{" "}
+                {amount}{" "}
+                <ion-icon
+                  name="remove"
+                  onClick={() => removeFromCart(name, price)}
+                ></ion-icon>
+                <ion-icon
+                  name="trash"
+                  onClick={() => removePlant(name)}
+                ></ion-icon>{" "}
               </div>
             ))}
           </ul>
