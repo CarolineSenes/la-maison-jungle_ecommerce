547c7247321f7c9588cd5b78851a461a3cb4efd4
SENES <caro67200@gmail.com>
2022-03-23 22:22:54 +0100

    update PlantItem : remove console.log test function

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -3,12 +3,9 @@ import "../styles/PlantItem.css";
 import CareScale from "../components/CareScale";
 
 const PlantItem = ({ name, cover, id, light, water, price }) => {
-  function handleClick(e) {
-    console.log(`Vous avez ajouté au panier la plante : `, e)
-  }
 
   return (
-    <li className="lmj-plant-item" key={id} onClick={()=>handleClick(name)}>
+    <li className="lmj-plant-item" key={id}>
       <span className='lmj-plant-item-price'>{price}€</span>
       <img className="lmj-plant-item-cover" src={cover} alt={name} />
       {name}
